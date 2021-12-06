9b2b1372b8dcc03eb6cbdb442aa22f33dea1a349
DingestSky <94025963+DingestSky@users.noreply.github.com>
2021-12-05 21:48:47 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,7 +1,7 @@
 '''
 @project       : Queens College CSCI 365/765 Computational Finance
 @Instructor    : Dr. Alex Pang
-@Student Name  : Zilong Zheng
+@Student Name  : Zilong Zheng, Yinan Du, Brian Luu
 @Date          : Nov 2021
 '''
 import enum
